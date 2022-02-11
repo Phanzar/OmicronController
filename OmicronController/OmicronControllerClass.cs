7eab9bfa6ec0e8106558467fec54fe4d7751a7ba
Phanzar <shadow.spy01@gmail.com>
2022-02-11 16:31:53 +0000

    Update

--- OmicronController/OmicronControllerClass.cs
+++ OmicronController/OmicronControllerClass.cs
@@ -38,6 +38,10 @@ namespace OmicronController
         static double anglec2 = 240;
         static double anglec3 = 120;
 
+        static double anglec1_original = 0;
+        static double anglec2_original = 240;
+        static double anglec3_original = 120;
+
 
         static bool current1_on = false;
         static bool current2_on = false;
@@ -189,52 +193,44 @@ namespace OmicronController
             Voltage3 = V3;
 
 
-            angle1_v_original = ANG1;
-            angle2_v_original = ANG2;
-            angle3_v_original = ANG3;
+            angle1 = ANG1;
+            angle2 = ANG2;
+            angle3 = ANG3;
 
-            if (ANG1>360)
-            {
-                angle1 = ANG1-360;
-            }
-            else
-            {
-                angle1 = ANG1;
-            }
 
-            if (ANG3 > 360)
-            {
-                angle3 = ANG3 - 360;
-            }
-            else
-            {
-                angle3 = ANG3;
-            }
+            angle1_v_original = angle1;
+            angle2_v_original = angle2;
+            angle3_v_original = angle3;
 
-            if (ANG2 > 360)
+            if (angle1 >= 360)
             {
-                angle2 = ANG2 - 360;
+                angle1 = angle1 - 360;
             }
-            else
+            if (angle2 >= 360)
             {
-                angle2 = ANG2;
+                angle2 = angle2 - 360;
+            }
+            if (angle3 >= 360)
+            {
+                angle3 = angle3 - 360;
             }
 
             //angle1 = ANG1;
             //angle2 = ANG2;
             //angle3 = ANG3;
 
-
-            
-
-            Console.WriteLine(engine.Exec(selectedDeviceId, string.Format(CultureInfo.InvariantCulture, "out:ana:v(1:1):a({0});f({1});p({2});wav(sin)", V1, FREQ, ANG1)));
-            Console.WriteLine(engine.Exec(selectedDeviceId, string.Format(CultureInfo.InvariantCulture, "out:ana:v(1:2):a({0});f({1});p({2});wav(sin)", V2, FREQ, ANG2)));
-            Console.WriteLine(engine.Exec(selectedDeviceId, string.Format(CultureInfo.InvariantCulture, "out:ana:v(1:3):a({0});f({1});p({2});wav(sin)", V3, FREQ, ANG3)));
+        
 
 
-            Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "V(1) ON: amplitude={0}; frequency={1}; anglec={2} ", V1, FREQ, ANG1));
-            Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "V(2) ON: amplitude={0}; frequency={1}; anglec={2}", V2, FREQ, ANG2));
-            Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "V(3) ON: amplitude={0}; frequency={1}; anglec={2}", V3, FREQ, ANG3));
+
+            Console.WriteLine(engine.Exec(selectedDeviceId, string.Format(CultureInfo.InvariantCulture, "out:ana:v(1:1):a({0});f({1});p({2});wav(sin)", V1, FREQ, angle1)));
+            Console.WriteLine(engine.Exec(selectedDeviceId, string.Format(CultureInfo.InvariantCulture, "out:ana:v(1:2):a({0});f({1});p({2});wav(sin)", V2, FREQ, angle2)));
+            Console.WriteLine(engine.Exec(selectedDeviceId, string.Format(CultureInfo.InvariantCulture, "out:ana:v(1:3):a({0});f({1});p({2});wav(sin)", V3, FREQ, angle3)));
+
+
+            Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "V(1) ON: amplitude={0}; frequency={1}; anglec={2} ", V1, FREQ, angle1));
+            Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "V(2) ON: amplitude={0}; frequency={1}; anglec={2}", V2, FREQ, angle2));
+            Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "V(3) ON: amplitude={0}; frequency={1}; anglec={2}", V3, FREQ, angle3));
 
 
             string result = engine.Exec(selectedDeviceId, "sys:status?");
@@ -248,47 +244,38 @@ namespace OmicronController
             current2 = I2;
             current3 = I3;
 
-            //anglec1 = ANG1;
-            //anglec2 = ANG2;
-            //anglec3 = ANG3;
+            anglec1 = ANG1;
+            anglec2 = ANG2;
+            anglec3 = ANG3;
 
-            if (ANG1 > 360)
-            {
-                anglec1 = ANG1 - 360;
-            }
-            else
-            {
-                anglec1 = ANG1;
-            }
+            anglec1_original = ANG1;
+            anglec2_original = ANG2;
+            anglec3_original = ANG3;
 
-            if (ANG3 > 360)
-            {
-                anglec3 = ANG3 - 360;
-            }
-            else
-            {
-                anglec3 = ANG3;
-            }
 
-            if (ANG2 > 360)
+            if (anglec1 >= 360)
             {
-                anglec2 = ANG2 - 360;
+                anglec1 = anglec1 - 360;
             }
-            else
+            if (anglec2 >= 360)
             {
-                anglec2 = ANG2;
+                anglec2 = anglec2 - 360;
+            }
+            if (anglec3 >= 360)
+            {
+                anglec3 = anglec3 - 360;
             }
 
 
 
-            Console.WriteLine(engine.Exec(selectedDeviceId, string.Format(CultureInfo.InvariantCulture, "out:ana:i(1:1):a({0});f({1});p({2});wav(sin)", I1, FREQ, ANG1)));
-            Console.WriteLine(engine.Exec(selectedDeviceId, string.Format(CultureInfo.InvariantCulture, "out:ana:i(1:2):a({0});f({1});p({2});wav(sin)", I2, FREQ, ANG2)));
-            Console.WriteLine(engine.Exec(selectedDeviceId, string.Format(CultureInfo.InvariantCulture, "out:ana:i(1:3):a({0});f({1});p({2});wav(sin)", I3, FREQ, ANG3)));
+            Console.WriteLine(engine.Exec(selectedDeviceId, string.Format(CultureInfo.InvariantCulture, "out:ana:i(1:1):a({0});f({1});p({2});wav(sin)", I1, FREQ, anglec1)));
+            Console.WriteLine(engine.Exec(selectedDeviceId, string.Format(CultureInfo.InvariantCulture, "out:ana:i(1:2):a({0});f({1});p({2});wav(sin)", I2, FREQ, anglec2)));
+            Console.WriteLine(engine.Exec(selectedDeviceId, string.Format(CultureInfo.InvariantCulture, "out:ana:i(1:3):a({0});f({1});p({2});wav(sin)", I3, FREQ, anglec3)));
 
 
-            Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "I(1) ON: current={0}; frequency={1}; angle={2}", I1, FREQ, ANG1));
-            Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "I(2) ON: current={0}; frequency={1}; angle={2}", I2, FREQ, ANG2));
-            Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "I(3) ON: current={0}; frequency={1}; angle={2}", I3, FREQ, ANG3));
+            Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "I(1) ON: current={0}; frequency={1}; angle={2}", I1, FREQ, anglec1));
+            Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "I(2) ON: current={0}; frequency={1}; angle={2}", I2, FREQ, anglec2));
+            Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "I(3) ON: current={0}; frequency={1}; angle={2}", I3, FREQ, anglec3));
 
             string result = engine.Exec(selectedDeviceId, "sys:status?");
             Console.WriteLine("System status: " + result);
@@ -305,40 +292,76 @@ namespace OmicronController
             current2 = I2;
             current3 = I3;
 
-            if (ANG1 > 360)
+            //anglec1 = 0;
+            //anglec2 = 240.0;
+            //anglec3 = 120.0;
+
+            //anglec1_original = 0;
+            //anglec2_original = 240.0;
+            //anglec3_original = 120.0;
+
+            //angle1 = ANG1;
+            //angle2 = ANG2;
+            //angle3 = ANG3;
+
+            //angle1_v_original = angle1;
+            //angle2_v_original = angle2;
+            //angle3_v_original = angle3;
+
+            //if (angle1 >= 360)
+            //{
+            //    angle1 = angle1 - 360;
+            //}
+            //if (angle2 >= 360)
+            //{
+            //    angle2 = angle2 - 360;
+            //}
+            //if (angle3 >= 360)
+            //{
+            //    angle3 = angle3 - 360;
+            //}
+
+            anglec1 = ANG1;
+            anglec2 = ANG2;
+            anglec3 = ANG3;
+
+            anglec1_original = anglec1;
+            anglec2_original = anglec2;
+            anglec3_original = anglec3;
+
+            if (anglec1 >= 360)
             {
-                anglec1 = ANG1 - 360;
+                anglec1 = anglec1 - 360;
             }
-            else
+            if (anglec2 >= 360)
             {
-                anglec1 = ANG1;
+                anglec2 = anglec2 - 360;
+            }
+            if (anglec3 >= 360)
+            {
+                anglec3 = anglec3 - 360;
             }
 
-            if (ANG3 > 360)
-            {
-                anglec3 = ANG3 - 360;
-            }
-            else
-            {
-                anglec3 = ANG3;
-            }
 
-            if (ANG2 > 360)
-            {
-                anglec2 = ANG2 - 360;
-            }
-            else
-            {
-                anglec2 = ANG2;
-            }
 
-            Console.WriteLine(engine.Exec(selectedDeviceId, string.Format(CultureInfo.InvariantCulture, "out:ana:i(1:1):a({0});f({1});p({2});wav(sin)", I1, FREQ, ANG1)));
-            Console.WriteLine(engine.Exec(selectedDeviceId, string.Format(CultureInfo.InvariantCulture, "out:ana:i(1:2):a({0});f({1});p({2});wav(sin)", I2, FREQ, ANG2)));
-            Console.WriteLine(engine.Exec(selectedDeviceId, string.Format(CultureInfo.InvariantCulture, "out:ana:i(1:3):a({0});f({1});p({2});wav(sin)", I3, FREQ, ANG3)));
+            Console.WriteLine(engine.Exec(selectedDeviceId, string.Format(CultureInfo.InvariantCulture, "out:ana:i(1:1):a({0});f({1});p({2});wav(sin)", I1, FREQ, anglec1)));
+            Console.WriteLine(engine.Exec(selectedDeviceId, string.Format(CultureInfo.InvariantCulture, "out:ana:i(1:2):a({0});f({1});p({2});wav(sin)", I2, FREQ, anglec2)));
+            Console.WriteLine(engine.Exec(selectedDeviceId, string.Format(CultureInfo.InvariantCulture, "out:ana:i(1:3):a({0});f({1});p({2});wav(sin)", I3, FREQ, anglec3)));
 
-            Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "I(1) ON: current={0}; frequency={1}; angle={2}", I1, FREQ, ANG1));
-            Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "I(2) ON: current={0}; frequency={1}; angle={2}", I2, FREQ, ANG2));
-            Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "I(3) ON: current={0}; frequency={1}; angle={2}", I3, FREQ, ANG3));
+
+            Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "I(1) ON: current={0}; frequency={1}; angle={2}", I1, FREQ, anglec1));
+            Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "I(2) ON: current={0}; frequency={1}; angle={2}", I2, FREQ, anglec2));
+            Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "I(3) ON: current={0}; frequency={1}; angle={2}", I3, FREQ, anglec3));
+
+
+            //Console.WriteLine(engine.Exec(selectedDeviceId, string.Format(CultureInfo.InvariantCulture, "out:ana:v(1:1):a({0});f({1});p({2});wav(sin)", Voltage1, FREQ, angle1)));
+            //Console.WriteLine(engine.Exec(selectedDeviceId, string.Format(CultureInfo.InvariantCulture, "out:ana:v(1:2):a({0});f({1});p({2});wav(sin)", Voltage2, FREQ, angle2)));
+            //Console.WriteLine(engine.Exec(selectedDeviceId, string.Format(CultureInfo.InvariantCulture, "out:ana:v(1:3):a({0});f({1});p({2});wav(sin)", Voltage3, FREQ, angle3)));
+
+
+            //Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "V(1) ON: amplitude={0}; frequency={1}; anglec={2} ", Voltage1, FREQ, angle1));
+            //Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "V(2) ON: amplitude={0}; frequency={1}; anglec={2}", Voltage2, FREQ, angle2));
+            //Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "V(3) ON: amplitude={0}; frequency={1}; anglec={2}", Voltage3, FREQ, angle3));
 
             inicio = DateTime.Now;
 
@@ -351,7 +374,7 @@ namespace OmicronController
 
                 TimeSpan tempo_decorrido = DateTime.Now - inicio;
 
-                energia_ja_injectada = (int)Math.Abs((tempo_decorrido.TotalSeconds / 3600.0 * (Math.Cos(Math.Abs(angle1 - anglec1) * 3.1415 / 180.0) * Voltage1 * current1 + Math.Cos(Math.Abs(angle2 - anglec2) * 3.1415 / 180.0) * Voltage2 * current2 + Math.Cos(Math.Abs(angle3 - anglec3) * 3.1415 / 180.0) * Voltage3 * current3)));
+                energia_ja_injectada = (int)Math.Abs((tempo_decorrido.TotalSeconds / 3600.0 * (Math.Cos(Math.Abs(angle1_v_original - anglec1_original) * 3.1415 / 180.0) * Voltage1 * current1 + Math.Cos(Math.Abs(angle2_v_original - anglec2_original) * 3.1415 / 180.0) * Voltage2 * current2 + Math.Cos(Math.Abs(angle3_v_original - anglec3_original) * 3.1415 / 180.0) * Voltage3 * current3)));
                 Console.WriteLine("Energy: " + energia_ja_injectada.ToString());
 
 
@@ -398,45 +421,76 @@ namespace OmicronController
             current2 = I2;
             current3 = I3;
 
-            if (ANG1 > 360)
+            //anglec1 = 0;
+            //anglec2 = 240.0;
+            //anglec3 = 120.0;
+
+            //anglec1_original = 0;
+            //anglec2_original = 240.0;
+            //anglec3_original = 120.0;
+
+            //angle1 = ANG1;
+            //angle2 = ANG2;
+            //angle3 = ANG3;
+
+            //angle1_v_original = angle1;
+            //angle2_v_original = angle2;
+            //angle3_v_original = angle3;
+
+            //if (angle1 >= 360)
+            //{
+            //    angle1 = angle1 - 360;
+            //}
+            //if (angle2 >= 360)
+            //{
+            //    angle2 = angle2 - 360;
+            //}
+            //if (angle3 >= 360)
+            //{
+            //    angle3 = angle3 - 360;
+            //}
+
+
+            anglec1 = ANG1;
+            anglec2 = ANG2;
+            anglec3 = ANG3;
+
+            anglec1_original = anglec1;
+            anglec2_original = anglec2;
+            anglec3_original = anglec3;
+
+            if (anglec1 >= 360)
             {
-                anglec1 = ANG1 - 360;
+                anglec1 = anglec1 - 360;
             }
-            else
+            if (anglec2 >= 360)
             {
-                anglec1 = ANG1;
+                anglec2 = anglec2 - 360;
             }
-
-            if (ANG3 > 360)
+            if (anglec3 >= 360)
             {
-                anglec3 = ANG3 - 360;
-            }
-            else
-            {
-                anglec3 = ANG3;
-            }
-
-            if (ANG2 > 360)
-            {
-                anglec2 = ANG2 - 360;
-            }
-            else
-            {
-                anglec2 = ANG2;
+                anglec3 = anglec3 - 360;
             }
 
 
 
+            Console.WriteLine(engine.Exec(selectedDeviceId, string.Format(CultureInfo.InvariantCulture, "out:ana:i(1:1):a({0});f({1});p({2});wav(sin)", I1, FREQ, anglec1)));
+            Console.WriteLine(engine.Exec(selectedDeviceId, string.Format(CultureInfo.InvariantCulture, "out:ana:i(1:2):a({0});f({1});p({2});wav(sin)", I2, FREQ, anglec2)));
+            Console.WriteLine(engine.Exec(selectedDeviceId, string.Format(CultureInfo.InvariantCulture, "out:ana:i(1:3):a({0});f({1});p({2});wav(sin)", I3, FREQ, anglec3)));
 
 
+            Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "I(1) ON: current={0}; frequency={1}; angle={2}", I1, FREQ, anglec1));
+            Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "I(2) ON: current={0}; frequency={1}; angle={2}", I2, FREQ, anglec2));
+            Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "I(3) ON: current={0}; frequency={1}; angle={2}", I3, FREQ, anglec3));
 
-            Console.WriteLine(engine.Exec(selectedDeviceId, string.Format(CultureInfo.InvariantCulture, "out:ana:i(1:1):a({0});f({1});p({2});wav(sin)", I1, FREQ, ANG1)));
-            Console.WriteLine(engine.Exec(selectedDeviceId, string.Format(CultureInfo.InvariantCulture, "out:ana:i(1:2):a({0});f({1});p({2});wav(sin)", I2, FREQ, ANG2)));
-            Console.WriteLine(engine.Exec(selectedDeviceId, string.Format(CultureInfo.InvariantCulture, "out:ana:i(1:3):a({0});f({1});p({2});wav(sin)", I3, FREQ, ANG3)));
+            //Console.WriteLine(engine.Exec(selectedDeviceId, string.Format(CultureInfo.InvariantCulture, "out:ana:v(1:1):a({0});f({1});p({2});wav(sin)", Voltage1, FREQ, angle1)));
+            //Console.WriteLine(engine.Exec(selectedDeviceId, string.Format(CultureInfo.InvariantCulture, "out:ana:v(1:2):a({0});f({1});p({2});wav(sin)", Voltage2, FREQ, angle2)));
+            //Console.WriteLine(engine.Exec(selectedDeviceId, string.Format(CultureInfo.InvariantCulture, "out:ana:v(1:3):a({0});f({1});p({2});wav(sin)", Voltage3, FREQ, angle3)));
 
-            Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "I(1) ON: current={0}; frequency={1}; angle={2}", I1, FREQ, ANG1));
-            Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "I(2) ON: current={0}; frequency={1}; angle={2}", I2, FREQ, ANG2));
-            Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "I(3) ON: current={0}; frequency={1}; angle={2}", I3, FREQ, ANG3));
+
+            //Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "V(1) ON: amplitude={0}; frequency={1}; anglec={2} ", Voltage1, FREQ, angle1));
+            //Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "V(2) ON: amplitude={0}; frequency={1}; anglec={2}", Voltage2, FREQ, angle2));
+            //Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "V(3) ON: amplitude={0}; frequency={1}; anglec={2}", Voltage3, FREQ, angle3));
 
             inicio = DateTime.Now;
 
@@ -449,7 +503,7 @@ namespace OmicronController
 
                 TimeSpan tempo_decorrido = DateTime.Now - inicio;
 
-                energia_ja_injectada = (int)Math.Abs((tempo_decorrido.TotalSeconds / 3600.0 * (Math.Sin(Math.Abs(angle1_v_original - anglec1) * Math.PI / 180.0) * Voltage1 * current1 + Math.Sin(Math.Abs(angle2_v_original - anglec2) * Math.PI / 180.0) * Voltage2 * current2 + Math.Sin(Math.Abs(angle3_v_original - anglec3) * Math.PI / 180.0) * Voltage3 * current3)));
+                energia_ja_injectada = (int)Math.Abs((tempo_decorrido.TotalSeconds / 3600.0 * (Math.Sin(Math.Abs(angle1_v_original - anglec1_original) * 3.1415 / 180.0) * Voltage1 * current1 + Math.Sin(Math.Abs(angle2_v_original - anglec2_original) * 3.1415 / 180.0) * Voltage2 * current2 + Math.Sin(Math.Abs(angle3_v_original - anglec3_original) * 3.1415 / 180.0) * Voltage3 * current3)));
                 Console.WriteLine("Energy: " + energia_ja_injectada.ToString());
 
 
